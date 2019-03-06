87f5d35ccc669364982dc2514db99636a83f7bcc
Abe D <iaryiou111@gmail.com>
2019-03-06 20:51:22 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -2,7 +2,11 @@ def reformat_languages(languages)
   new_hash = {}
   languages.keys.each do |lstyle|
     languages[lstyle].keys.each do |language|
-      new_hash[language] = {type: languages[lstyle][language][:type], style: lstyle}
+      if new_hash.keys.include?(language) == false
+        new_hash[language] = {type: languages[lstyle][language][:type], style: [lstyle]}
+      else
+        new_hash[language][:style].push(lstyle)
+      end
     end
   end
   new_hash
