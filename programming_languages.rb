ef1bcb565904176308c288be352f0337236a5f06
Abe D <iaryiou111@gmail.com>
2019-03-06 20:45:28 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,5 +1,9 @@
 def reformat_languages(languages)
   new_hash = {}
   languages.keys.each do |style|
-    languages
+    languages[style].keys.each do |language|
+      new_hash[language] = {type: language_type, style: languages[style][language][:type] }
+    end
+  end
+  new_hash
 end
